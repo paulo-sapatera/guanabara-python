d549e2ce44255fcbbe54aeb70e2e80e2e763377d
Paulo Sapatera <sapateraneto@hotmail.com>
2021-11-16 22:35:51 -0300

    alteração

--- ex017.py
+++ ex017.py
@@ -2,4 +2,4 @@ co = float(input("Comprimento do cateto oposto: "))
 ca = float(input("Comprimento do cateto adjacente: "))
 hi = (co ** 2 + ca **2) ** (1/2)
 print ("A hipotenusa vai medir {:.2f}".format (hi))
-#da pra fazer com "math" tambem, importando e usando hipot!
+#da pra fazer com "math" tambem, importando e usando hipot
