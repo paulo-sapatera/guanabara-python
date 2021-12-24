7d1fc718f0a2424463623c4b8cc23fa1209a9ec5
Paulo Sapatera <sapateraneto@hotmail.com>
2021-12-24 17:08:11 -0300

    commit

--- ex034.py
+++ ex034.py
@@ -1,16 +1,6 @@
-a = int(input("Digite o primeiro valor: "))
-b = int(input("Digite o segundo valor: "))
-c = int(input("Digite o terceiro valor: "))
-#Verificando quem é o menor
-menor = a 
-if b<a and b<c:
-    menor = b
-if c<a and c<b:
-    menor = c
-#Verificando quem é o maior
-maior = a
-if b>a and b>c:
-    maior = b
-if c>a and c>b:
-    maior = c
-print ("O maior valor digitado foi {} e o menor foi {}".format(maior,menor))
+salario = float(input("Salario do funcionario: "))
+if salario <= 1250:
+    novo = salario + (salario*15/100) #pra ler porcentagem é 15 por 100to do salario
+else:
+    novo = salario + (salario*10/100)
+print("Quem ganhava RS{:.2f} , passa a ganhar R${:.2f}".format(salario,novo))
